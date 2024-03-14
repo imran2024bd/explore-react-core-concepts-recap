0a7a16a5c3f2fc75e7912ad9899e37b4e4af4b6c
imran2024bd <imranitbd34@gmail.com>
2024-03-14 15:41:02 +0600

    39-9 Explore React Official Documentation

--- src/Post.jsx
+++ src/Post.jsx
@@ -15,4 +15,13 @@ export default function Post({post}){
 
 /**
  * 39-8 Thinking In React Way
+ * 39-9 Explore React Official Documentation
+ * https://react.dev/learn
+ * You will learn
+How to create and nest components
+How to add markup and styles
+How to display data
+How to render conditions and lists
+How to respond to events and update the screen
+How to share data between components
  */
